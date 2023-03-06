ec1a44bd22917ad3e66c0dbf8eddfe16cf016b45
José Gerardo Ríos Sarango <73481008+Gerardo-rios@users.noreply.github.com>
2023-03-06 12:51:07 -0500

    oberservers hotfix on onboarding screen (#26)

--- lib/features/authentication/onboarding/onboarding_screen.dart
+++ lib/features/authentication/onboarding/onboarding_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:ptt_rtmb/core/controlers/onboarding_screen_controller.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -14,49 +13,44 @@ class OnBoardingScreen extends StatelessWidget {
 
     return Scaffold(
       body: Stack(alignment: Alignment.center, children: [
-        Obx(
-          () => LiquidSwipe(
-            pages: obController.pages,
-            liquidController: obController.controller,
-            onPageChangeCallback: obController.OnPageChangeCallback,
-            slideIconWidget: const Icon(Icons.arrow_back_ios),
-            enableSideReveal: true,
-          ),
+        LiquidSwipe(
+          pages: obController.pages,
+          liquidController: obController.controller,
+          onPageChangeCallback: obController.OnPageChangeCallback,
+          slideIconWidget: const Icon(Icons.arrow_back_ios),
+          enableSideReveal: true,
         ),
         Positioned(
           bottom: 40,
-          child: Obx(
-            () => OutlinedButton(
-              onPressed: () => obController.animateToNextSlide(context),
-              style: ElevatedButton.styleFrom(
-                side: const BorderSide(color: Colors.black26),
-                shape: const CircleBorder(),
-                padding: const EdgeInsets.all(20),
-                onPrimary: Colors.white,
-              ),
-              child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 163, 157, 157),
-                      shape: BoxShape.circle),
-                  child: const Icon(Icons.arrow_forward_ios)),
+          child: OutlinedButton(
+            onPressed: () => obController.animateToNextSlide(context),
+            style: ElevatedButton.styleFrom(
+              side: const BorderSide(color: Colors.black26),
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(20),
+              onPrimary: Colors.white,
             ),
+            child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 163, 157, 157),
+                    shape: BoxShape.circle),
+                child: const Icon(Icons.arrow_forward_ios)),
           ),
         ),
         Positioned(
-            top: 20,
-            right: 20,
-            child: Obx(
-              () => TextButton(
-                onPressed: () => obController.skip(context),
-                child: const Text(
-                  "Saltar",
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            )),
+          top: 20,
+          right: 20,
+          child: TextButton(
+            onPressed: () => obController.skip(context),
+            child: const Text(
+              "Saltar",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
         Obx(
           () => Positioned(
               bottom: 10,
